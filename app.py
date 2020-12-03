2d3d57519b0e5380c1bb1d5a0bf8ed83a9e02714
@jonathanlei <jonathanlei98@gmail.com>
2020-12-02 19:21:33 -0500

    finished the main functions

--- app.py
+++ app.py
@@ -7,3 +7,18 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def create_homepage():
+    """ returns homepage html with a question form"""
+    prompts = silly_story.prompts
+    return render_template("questions.html", prompts=prompts)
+
+
+@app.route("/story")
+def generate_story():
+    """ generate a story from form data upon form submission '"""
+    answers = request.args
+    story = silly_story.generate(answers)
+    return render_template("story.html", story=story)
